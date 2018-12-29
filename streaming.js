36c97b1c6fdda0382267ce77e0ea9cba598785dc
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-29 06:26:52 -0800

    Update streaming.js

--- streaming.js
+++ streaming.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const adminprefix = "$";
-const developers = ['439393453332234243'];
+const developers = ['403269800819818498'];
 
 client.on('ready', () => {
     console.log(`Logged as ${client.user.tag}`)
